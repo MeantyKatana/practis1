ea4ed8cb13abb3d8842dc064d4f62d7d81e07a8b
MeantyKatana <n.petrishen@istu.edu.ua>
2024-04-09 16:42:55 +0300

    first

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+@Injectable()
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  check(email: string) {
+    const user = this.usersService.users.find(user => user.email === email);
+    if (!user) {
+        throw new NotFoundException(`Користувач з такою поштою: ${email}} не знайдений!`);
+    }
+    return user;    
+  }
+}
